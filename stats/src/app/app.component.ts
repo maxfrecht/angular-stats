152a1534536e3a9548ce49245f151471bed8b3ca
Maxfrecht <max.frecht@gmail.com>
2021-05-27 11:49:15 +0200

    Exercice 4

--- stats/src/app/app.component.ts
+++ stats/src/app/app.component.ts
@@ -4,9 +4,17 @@ import { Statistique } from './models/statistique';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  public stat1 = new Statistique('1234', 'habitants StEtienne', '171 057', 'SUCCESS')
-  public stat2 = new Statistique('5678', 'habitants Paris', '2,161 millions', 'WARNING')
+  public stats: Statistique[] = [
+    new Statistique('1234', 'habitants StEtienne', '171 057', 'SUCCESS'),
+    new Statistique('5678', 'habitants Paris', '2,161 millions', 'WARNING'),
+  ];
+
+  constructor() {
+    setTimeout(() => {
+      this.stats.push(new Statistique('9101', 'habitants Montpellier', '277 639', 'SUCCESS'))
+    }, 4000);
+  }
 }
